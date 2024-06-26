0b2455f1a890e20c8f59406ed8c666b4c539df45
Nicole <nlinonicole@gmail.com>
2024-06-25 21:04:44 -0300

    interfaces

--- Pacotes/src/apps/ServicoMensagemInstantanea.java
+++
@@ -1,9 +0,0 @@
-package apps;
-public abstract class ServicoMensagemInstantanea {
-    public abstract void enviarMensagem();
-    public abstract void receberMensagem();
-
-    protected void validarConexcaoInternet() {
-        System.out.println("Validando conexcao internet");
-    }
-}
